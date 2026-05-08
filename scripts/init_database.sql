5df1a75277c6fb6d336e69233a9e772a2b3b2d79
Stefan Janicijevic <janicijevic.stefi@gmail.com>
2026-05-08 12:42:32 +0200

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -19,15 +19,21 @@ BEGIN
     ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
     DROP DATABASE DataWarehouse;
 END;
+GO
 
 -- Create the 'DataWarehouse' database
 CREATE DATABASE DataWarehouse;
-
+GO
+	
 USE DataWarehouse;
-
+GO
+	
 -- Create Schemas
 CREATE SCHEMA bronze;
-
+GO
+	
 CREATE SCHEMA silver;
-
+GO
+	
 CREATE SCHEMA gold;
+GO
