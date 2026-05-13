b111b29ab460fdb5374e35310a43d60067084c6b
Stefan Janicijevic <janicijevic.stefi@gmail.com>
2026-05-13 11:53:53 +0200

    Enhance quality check for gold.fact_sales
    Added a query to check for missing customer and product keys in fact_sales.

--- tests/quality_checks_gold.sql
+++ tests/quality_checks_gold.sql
@@ -88,6 +88,7 @@ HAVING COUNT(*) > 1;
 -- ====================================================================
 -->>> Checking gold.fact_sales
 SELECT * FROM gold.fact_sales;
+
 SELECT * FROM gold.fact_sales f
 LEFT JOIN gold.dim_customers c ON c.customer_key = f.customer_key
 LEFT JOIN gold.dim_products p ON p.product_key = f.product_key
